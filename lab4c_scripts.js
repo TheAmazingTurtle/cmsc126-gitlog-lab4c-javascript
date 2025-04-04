ccf8eee668ca00cca4a813a934b66c1208e61cde
Kent Francis Genilo <kegenilo@up.edu.ph>
2025-04-04 13:03:05 +0800

    removed block of code

--- lab4c_scripts.js
+++ lab4c_scripts.js
@@ -171,16 +171,6 @@ function display_list(){
     tableContainer.appendChild(table);
 }
 
-// function displayStudent(student){
-//     let currentRow = document.getElementById("studentDisplayTable").insertRow();
-
-//     currentRow.insertCell().outerHTML = `<td class="allStudentNum">${student.studentNumber}</td>`;
-//     currentRow.insertCell().outerHTML = `<td class="allName">${student.name}</td>`;
-//     currentRow.insertCell().outerHTML = `<td class="allAge">${student.age}</td>`;
-//     currentRow.insertCell().outerHTML = `<td class="allEmail">${student.email}</td>`;
-//     currentRow.insertCell().outerHTML = `<td class="allCourse">${student.course}</td>`;
-// }
-
 // Minmin : code assistance by ChatGPT, OpenAI, April 3, 2025, https://chatgpt.com/c/67eea4e1-7724-8005-8dcd-b6e2bcc3b511
 
 function find_student(event){
